50eea8a15354c2b9430e833875b9cf39e05d5fc9
Ivan Yakimov <yoowhi@gmail.com>
2021-05-09 20:58:56 +0300

    Logger improved

--- src/logger.ts
+++ src/logger.ts
@@ -1,30 +1,33 @@
 import { LogLevel } from "./types";
+import { logLevel as currentLevel } from "./config.json"
 
 
 export function applog(message: string, logLevel: LogLevel, object?: Object) {
-    let log = console.log;
-    let text = "";
-    switch (logLevel) {
-        case LogLevel.DEBUG:
-            text = "DEBUG: " + message;
-            break;
-        case LogLevel.INFO:
-            text = "INFO: " + message;
-            break;
-        case LogLevel.WARNING:
-            text = "WARNING: " + message;
-            break;
-        case LogLevel.ERROR:
-            log = console.error;
-            text = "ERROR: " + message;
-            break;
-        default:
-            throw "ERROR: unknown log level";
-    }
-    if (object) {
-        text += ". Object: ";
-        log(text, object);
-    } else {
-        log(text);
-    }
+    if (logLevel >= currentLevel) {
+        let log = console.log;
+        let text = "";
+        switch (logLevel) {
+            case LogLevel.DEBUG:
+                text = "DEBUG: " + message;
+                break;
+            case LogLevel.INFO:
+                text = "INFO: " + message;
+                break;
+            case LogLevel.WARNING:
+                text = "WARNING: " + message;
+                break;
+            case LogLevel.ERROR:
+                log = console.error;
+                text = "ERROR: " + message;
+                break;
+            default:
+                throw "ERROR: unknown log level";
+        }
+        if (object) {
+            text += ". Object: ";
+            log(text, object);
+        } else {
+            log(text);
+        }
+    }d
 }
